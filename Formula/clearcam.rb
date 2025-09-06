805b5b5981f4d38b49d722c103e417f589665215
Rory Clear <45880510+roryclear@users.noreply.github.com>
2025-09-06 21:29:10 +0800

    Update clearcam.rb

--- Formula/clearcam.rb
+++ Formula/clearcam.rb
@@ -1,7 +1,7 @@
 class Clearcam < Formula
   desc "NVR with AI object detection."
   homepage "https://github.com/roryclear/clearcam"
-  url "https://github.com/roryclear/clearcam/releases/download/0.1.0/clearcam-0.1.0-macos.zip"
+  url "https://github.com/roryclear/clearcam/releases/download/0.1.1/clearcam-0.1.1-macos.zip"
   sha256 "cfe653452bbb04f717c01ed501183f78450a59724aab80fbfd238a3c9d3addc5"
   license "AGPL"
   depends_on "ffmpeg"
